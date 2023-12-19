11c115579e4e4c26067b790c5deda60b0f86b152
Vinícius <vvv@cesar.school>
2023-12-19 13:39:18 -0300

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-nomeApp="helloreact"
+PROCESS_NAME="helloreact"
 
-sudo supervisorctl stop $nomeApp
+if pgrep -f "$PROCESS_NAME" > /dev/null; then
+    echo "Processo $PROCESS_NAME está em execução. Parando..."
+    
+    sudo supervisorctl stop "$PROCESS_NAME"
+    sleep 5
+
+    echo "Processo $PROCESS_NAME parado com sucesso."
+else
+    echo "Processo $PROCESS_NAME não está em execução. Nada a parar."
+    exit 0
+fi
