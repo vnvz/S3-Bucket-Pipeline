8a25b5766194b18209cbb2a3c8bb9e9a5c1b0aec
Vinícius <vvv@cesar.school>
2023-12-21 14:57:31 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,9 @@ nomeApp="helloreact"
 cd /appdir
 npm install
 
+process=`sudo lsof -i :9001`
+pid=`echo $process | grep -oP '\b\d+\b' | head -n 1`
+sudo kill -9 $pid
+
 sudo supervisord
 sudo supervisorctl start $nomeApp
